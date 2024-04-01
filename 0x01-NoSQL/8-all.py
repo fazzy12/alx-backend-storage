ef6d78308cb4d4b502c850f367b5968c02de0980
fazzy12 <kaluifeanyi00@gmail.com>
2024-04-01 19:51:56 +0100

    Add script to list all MongoDB databases

--- 0x01-NoSQL/8-all.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" List all documents in Python"""
-
-
-def list_all(mongo_collection):
-    """
-    Function that lists all documents in a collection
-    """
-    return mongo_collection.find()
--- 0x01-NoSQL/9-insert_school.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""9. Insert a document in Python"""
-
-
-def insert_school(mongo_collection, **kwargs):
-    """
-    Function that inserts a new document in a collection based on kwargs
-    """
-    return mongo_collection.insert_one(kwargs).inserted_id
