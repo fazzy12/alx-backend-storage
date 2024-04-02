86831721e4a193188396d2de165f9dc4a557fb6a
fazzy12 <kaluifeanyi00@gmail.com>
2024-04-02 21:30:06 +0100

    Add solution for listing all documents in a MongoDB collection in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""
+    Lists all documents in a MongoDB collection.
+"""
 
 def list_all(mongo_collection):
     """
-    Lists all documents in a MongoDB collection.
     Args:
         mongo_collection: pymongo collection object.
     Returns:
