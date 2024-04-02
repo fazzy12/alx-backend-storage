011867bc624b1ee75d4bdb7391d62ba821812df5
fazzy12 <kaluifeanyi00@gmail.com>
2024-04-02 23:43:57 +0100

    feat: Implement function to return top students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,17 +2,26 @@
 """function that returns all students sorted by average score:
 """
 
-
 def top_students(mongo_collection):
-    """Returns all students sorted by average score."""
-    students = mongo_collection.find()
-
-    top_students = []
-    for student in students:
-        scores = [topic['score'] for topic in student['topics']]
-        average_score = sum(scores) / len(scores) if scores else 0
-        student['averageScore'] = round(average_score, 2)
-        top_students.append(student)
-
-    top_students.sort(key=lambda x: x['averageScore'], reverse=True)
-    return top_students
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
