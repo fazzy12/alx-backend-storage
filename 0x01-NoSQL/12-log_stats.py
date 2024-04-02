8423a03df445de2ef586dfaf7da39f681b340ea1
fazzy12 <kaluifeanyi00@gmail.com>
2024-04-02 23:27:09 +0100

    Python script to calculate statistics about Nginx logs stored in MongoDB,

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
-"""This script that provides some stats about Nginx logs stored in MongoDB"""
+"""This script provides some stats about Nginx logs stored in MongoDB"""
 
+import sys
 from pymongo import MongoClient
 
 
-def nginx_stats_check():
-    """ provides some stats about Nginx logs stored in MongoDB:"""
-    client = MongoClient()
-    collec_nginx = client.logs.nginx
+def nginx_stats_check(db_name):
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    try:
+        client = MongoClient()
+        db = client[db_name]
+        collec_nginx = db.nginx
 
-    num_of_docs = collec_nginx.count_documents({})
-    print("{} logs".format(num_of_docs))
-    print("Methods:")
-    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods_list:
-        method_count = collec_nginx.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status = collec_nginx.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status))
+        num_of_docs = collec_nginx.count_documents({})
+        print(f"{num_of_docs} logs")
+
+        print("Methods:")
+        methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for method in methods_list:
+            method_count = collec_nginx.count_documents({"method": method})
+            print(f"\tmethod {method}: {method_count}")
+
+        status = collec_nginx.count_documents(
+            {"method": "GET", "path": "/status"})
+        print(f"{status} status check")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    nginx_stats_check()
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <database_name>")
+        sys.exit(1)
+
+    db_name = sys.argv[1]
+    nginx_stats_check(db_name)
